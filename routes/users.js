af2e591fbb09c6c6072896425e323f01b00742df
Klint Tiley <kctiley@gmail.com>
2015-12-03 14:23:13 -0700

    Refactored for cleaner routes

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,8 @@ router.post('/signUp', function(req, res, next) {
     .createUser(req.body.email, req.body.password, req.body.passConf)
     .then(
       function(){ res.redirect('/') },
-      function(err){ res.render('signUp', {errors: err.errors}) }
+      function(data){ 
+        res.render('signUp', {errors: data.errors}) }
     );
 });
 
@@ -26,51 +27,16 @@ router.get('/logIn', function(req, res, next) {
   res.render('logIn');
 });
 
-// router.post('/login', function(req, res, next) {
-//   var errors = [];
-//   if(!req.body.email) {
-//     errors.push('Email is required');
-//   }
-//   if(!req.body.password) {
-//     errors.push('Password is required');
-//   }
-//   if(errors.length) {
-//     res.render('logIn', {errors: errors});
-//   }
-//   else {
-//     Helper.signin(req.body.email, req.body.password).then(function (data) {
-//       if(data.errors) {
-//         res.render('login', {errors: data.errors});
-//       }
-//       else {
-//         req.session.email = req.body.email;
-//         res.redirect('/users');
-//       }
-//     });
-//   }  
-// });
-
 router.post('/login', function(req, res, next) {
   var errors = [];
-  if(!req.body.email) {
-    errors.push('Email is required');
-  }
-  if(!req.body.password) {
-    errors.push('Password is required');
-  }
-  if(errors.length) {
-    res.render('logIn', {errors: errors});
-  }
-  else {
-    Helper.logInUser(req.body.email, req.body.password)
-    .then(function (data) {
-      // req.session.email =  req.body.email;
-      res.redirect('/');
-    }, function(err){
-      errors = err.errors;
-      res.render('login', {errors: errors})
-    });
-  }  
+  Helper.logInUser(req.body.email, req.body.password)
+  .then(
+    function (data){res.redirect('/');
+    }, 
+    function(err){
+      errors.push(err);
+      res.render('login', {errors: err.errors})
+  }); 
 });
 
 router.get('/logout', function(req, res, next) {
